6a9580c273a38c89e2322387b3c7ac250bc96f2f
Mathias Hansson <drokkie@gmail.com>
2019-12-02 21:04:44 -0800

    turn it off for real

--- subnets.tf
+++ subnets.tf
@@ -33,21 +33,21 @@
 
 
 # Azure subnet setup. 
-resource "azurerm_virtual_network" "blue_virtual_network" {
-    address_space = ["10.0.0.0/16"]
-    location = "West US"
-    name = "bluevirtualnetwork"
-    resource_group_name = "${azurerm_resource_group.azy_network.name}"
-    dns_servers = ["10.0.0.4","10.0.0.5"]
-    subnet {
-        name = "subnet1"
-        address_prefix = "10.0.1.0/24"
-    }
-    subnet {
-        name = "subnet2"
-        address_prefix = "10.0.2.0/24"
-    }
-    # tags {
-    #     environment = "blue-world-finder"
-    # }
-}
+# resource "azurerm_virtual_network" "blue_virtual_network" {
+#     address_space = ["10.0.0.0/16"]
+#     location = "West US"
+#     name = "bluevirtualnetwork"
+#     resource_group_name = "${azurerm_resource_group.azy_network.name}"
+#     dns_servers = ["10.0.0.4","10.0.0.5"]
+#     subnet {
+#         name = "subnet1"
+#         address_prefix = "10.0.1.0/24"
+#     }
+#     subnet {
+#         name = "subnet2"
+#         address_prefix = "10.0.2.0/24"
+#     }
+#     # tags {
+#     #     environment = "blue-world-finder"
+#     # }
+# }
