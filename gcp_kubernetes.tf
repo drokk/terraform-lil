93ba9963baae411c6a35646fcc2186227ba4c54f
Mathias Hansson <drokkie@gmail.com>
2019-12-06 07:47:07 -0800

    shutdown

--- gcp_kubernetes.tf
+++ gcp_kubernetes.tf
@@ -1,30 +1,30 @@
-resource "google_container_cluster" "kubey" {
-    name = "kubeycluster"
-    location = "us-west1-a"
-    initial_node_count = "1"
+# resource "google_container_cluster" "kubey" {
+#     name = "kubeycluster"
+#     location = "us-west1-a"
+#     initial_node_count = "1"
 
-    node_locations = [
-        "us-west1_b"
-    ]
+#     node_locations = [
+#         "us-west1_b"
+#     ]
 
-    master_auth {
-        password = "password"
-        username = "username"
-    }
+#     master_auth {
+#         password = "password-16-or-more-characters"
+#         username = "username"
+#     }
 
-    node_config {
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/compute",
-            "https://www.googleapis.com/auth/devstorage.read_only",
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ]
-    }
-    # labels {
-    #     this-is-for = "dev-cluster"
-    # }
+#     node_config {
+#         oauth_scopes = [
+#             "https://www.googleapis.com/auth/compute",
+#             "https://www.googleapis.com/auth/devstorage.read_only",
+#             "https://www.googleapis.com/auth/logging.write",
+#             "https://www.googleapis.com/auth/monitoring",
+#         ]
+#     }
+#     # labels {
+#     #     this-is-for = "dev-cluster"
+#     # }
 
-    # tags = ["dev","work"]
-    # }
+#     # tags = ["dev","work"]
+#     # }
   
-}
+# }
--- servers.tf
+++ servers.tf
@@ -1,49 +1,49 @@
-resource "google_compute_instance" "firstserver" {
-    name = "thefirstserver"
-    machine_type = "n1-standard-1"
-    zone = "us-west1-a"
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-10"
+# resource "google_compute_instance" "firstserver" {
+#     name = "thefirstserver"
+#     machine_type = "n1-standard-1"
+#     zone = "us-west1-a"
+#     boot_disk {
+#         initialize_params {
+#             image = "debian-cloud/debian-10"
 
 
-        }
-    }
-  network_interface{
-      subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
-    access_config {}
-    }
-    # metadata {
-    #     foo = "bar"
-    # }
+#         }
+#     }
+#   network_interface{
+#       subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+#     access_config {}
+#     }
+#     # metadata {
+#     #     foo = "bar"
+#     # }
 
-    service_account {
-        scopes = ["userinfo-email","compute-ro","storage-ro"]
+#     service_account {
+#         scopes = ["userinfo-email","compute-ro","storage-ro"]
 
-    }
-}
+#     }
+# }
 
 
-# AWS server definition 
+# # AWS server definition 
 
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
+# data "aws_ami" "ubuntu" {
+#     most_recent = true
+#     filter {
+#         name = "name"
+#         values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#     }
+#     filter {
+#         name = "virtualization-type"
+#         values = ["hvm"]
+#     }
 
-    owners = ["099720109477"]
+#     owners = ["099720109477"]
 
-}
+# }
 
-resource "aws_instance" "secondserver" {
-    ami = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.subnet2.id}"
+# resource "aws_instance" "secondserver" {
+#     ami = "${data.aws_ami.ubuntu.id}"
+#     instance_type = "t2.micro"
+#     subnet_id = "${aws_subnet.subnet2.id}"
   
-}
+# }
