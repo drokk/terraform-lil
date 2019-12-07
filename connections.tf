e523a1ab90c8361096385b88e96d10af20a72cae
Mathias Hansson <drokkie@gmail.com>
2019-12-07 07:26:43 -0800

    formatting

--- connections.tf
+++ connections.tf
@@ -1,32 +1,32 @@
 provider "google" {
-    credentials = "${file("../account.json")}"
-    project = "wide-plating-260514"
-    region = "us-west1"
+  credentials = "${file("../account.json")}"
+  project     = "wide-plating-260514"
+  region      = "us-west1"
 }
 
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 provider "azurerm" {
-    subscription_id = "${var.subscription_id}"
-    client_id = "${var.client_id}"
-    client_secret = "${var.client_secret}"
-    tenant_id = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 variable "subscription_id" {
-  
+
 }
 
 variable "client_id" {
-  
+
 }
 
 variable "client_secret" {
-  
+
 }
 
 variable "tenant_id" {
-  
+
 }
